bbd800d4a7ba688085c3b6401de60d96aa32a80b
AMNA164 <n95783678>
2026-09-05 14:48:55 +0500

    Add Botpress webchat

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const menuToggle = document.querySelector('[data-menu-toggle]');
 const navigation = document.querySelector('[data-nav]');
 
+const botpressInject = document.createElement('script');
+botpressInject.src = 'https://cdn.botpress.cloud/webchat/v3.7/inject.js';
+document.head.appendChild(botpressInject);
+
+const botpressConfig = document.createElement('script');
+botpressConfig.src = 'https://files.bpcontent.cloud/2026/07/26/10/20260726100403-QO3OBXAF.js';
+botpressInject.addEventListener('load', () => document.head.appendChild(botpressConfig));
+
 const ownerPhone = document.querySelector('a[href="tel:+92510000000"]');
 if (ownerPhone) {
   ownerPhone.href = 'tel:+923223834771';
